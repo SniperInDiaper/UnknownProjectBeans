dfde92190a57deca1b8dadefc89a5502c7a860cc
SniperInDiaper <mjeedbu@gmail.com>
2022-07-27 08:23:43 +0300

    Update #5 Majeed

--- GameProjectUnknown/Assets/Scripts/PlayerMovements.cs
+++ GameProjectUnknown/Assets/Scripts/PlayerMovements.cs
@@ -5,9 +5,12 @@ public class PlayerMovements : MonoBehaviour
     [SerializeField]private float speed;
     [SerializeField]private float jumpDistance; 
     [SerializeField]private LayerMask groundLayer; //LayerMask are used to separate collision entetities 
+    [SerializeField]private LayerMask wallLayer; //LayerMask are used to separate collision entetities
     private Rigidbody2D body; //Used for rigid body and physics over the player's body
     private Animator animate; //Variable for the animation 
     private BoxCollider2D boxCollider; //Initiate for box collider
+    private float wallJumpCooldown; //responcible for creating delay between wall jumps
+    private float horizantalInput;
 
 
     //awake method is activated once at the beginning of the game
@@ -21,9 +24,9 @@ public class PlayerMovements : MonoBehaviour
 
     }
     //update method is for things that must be checked every frame 
-    private void Update(){
-        float horizantalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(horizantalInput * speed,body.velocity.y);
+    private void Update()
+    {
+         horizantalInput = Input.GetAxis("Horizontal");
 
         //flip Player when moving left and right
         if(horizantalInput> 0.01f)
@@ -32,9 +35,26 @@ public class PlayerMovements : MonoBehaviour
             transform.localScale = new Vector3(-1,1,1);
 
 
-        //jumping command
-        if(Input.GetKey(KeyCode.Space) && isGrounded())
-           Jump();
+       
+
+        //wall jumps logic
+        if(wallJumpCooldown > 0.2f)
+        {
+             body.velocity = new Vector2(horizantalInput * speed,body.velocity.y);
+
+             if(onWall() && !isGrounded())
+             {
+                body.gravityScale = 0;
+                body.velocity = Vector2.zero;
+             }else 
+                    body.gravityScale = 1.75f;
+            
+                //jumping command
+             if(Input.GetKey(KeyCode.Space) )
+                Jump();
+
+        }else 
+                wallJumpCooldown += Time.deltaTime;
 
 
 
@@ -46,9 +66,25 @@ public class PlayerMovements : MonoBehaviour
 
     }
         //Jumping function
-    private void Jump(){
-         body.velocity = new Vector2(body.velocity.x, jumpDistance);
-         animate.SetTrigger("Jump");
+    private void Jump()
+    {   
+        if(isGrounded())
+        {
+            body.velocity = new Vector2(body.velocity.x, jumpDistance);
+             animate.SetTrigger("Jump");
+        }
+        else if(onWall() && !isGrounded())
+        {
+            if(horizantalInput == 0)
+            {
+                 body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0);
+                 transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x) , transform.localScale.y, transform.localScale.z);
+            }
+            else
+                 body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6);
+
+            wallJumpCooldown = 0;
+        }
     }
         /*This is a function to detect if the player is on the ground by checking
          if there is collision between the player and gameObject tagged with Ground*/
@@ -64,6 +100,14 @@ public class PlayerMovements : MonoBehaviour
         BoxCasting is used to check the collosion box instead of casting a ray.
          the parameters are (Origin of the box, Size of the box, Angle of the box, Direction, distance of the vertiual box from the object, Layer mask)*/
         RaycastHit2D ray = Physics2D.BoxCast(boxCollider.bounds.center,boxCollider.bounds.size,0,Vector2.down, 0.1f, groundLayer);
+        return ray.collider != null;
+    }
+    private bool onWall()
+    {
+        /* RaycastHit2D is used to cast a light of the object to determine something.
+        BoxCasting is used to check the collosion box instead of casting a ray.
+         the parameters are (Origin of the box, Size of the box, Angle of the box, Direction, distance of the vertiual box from the object, Layer mask)*/
+        RaycastHit2D ray = Physics2D.BoxCast(boxCollider.bounds.center,boxCollider.bounds.size,0,new Vector2(transform.localScale.x,0), 0.1f, wallLayer);
         return ray.collider != null;
     }
 
